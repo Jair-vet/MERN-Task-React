2560b90b8bdacc835e86d5834354c6344a90c29f
Jair <jairaceves56@gmail.com>
2021-11-10 08:32:56 -0600

    Creamos el campo del Email en el Login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,32 @@
 import React from 'react'
 
 const Login = () => {
+
+    
+    
+    const onChange = () => {
+        
+    }
+
+
     return ( 
-        <h1>Desde Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar Sesión</h1>
+                <form>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Email"
+                            onChange={onChange}
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
      );
 }
  

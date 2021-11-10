f0fa21e07b158a187e24625e9aa5c61f6ebc20e5
Jair <jairaceves56@gmail.com>
2021-11-10 09:26:09 -0600

    Agregamod

--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -1,9 +1,111 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 
 const NuevaCuenta = () => {
+
+    // State para iniciar sesión
+    const [usuario, guardarUsuario] = useState({
+        email: '',
+        password: '',
+    });
+
+    // Extraer de Usuario
+    const { email, password } =  usuario
+    
+    
+    const onChange = e => {
+        guardarUsuario({
+            ...usuario,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    // Cuando el usuario quiere iniciar sesión
+    const onSubmit = e => {
+        e.preventDefault();
+
+
+        // Validar campos vacios
+
+        // Pasarlo al action
+
+
+    }
+
     return ( 
-        <h1>Desde Nueva Cuenta</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Obtener Cuenta</h1>
+                <form
+                    onSubmit={onSubmit}
+                >
+
+                 {/* Agremar el Nombre */}
+                    <div className="campo-form">
+                        <label htmlFor="nombre">Nombre</label>
+                        <input 
+                            type="text"
+                            id="nombre"
+                            name="nombre"
+                            placeholder="Nombre"
+                            value={email}
+                            onChange={onChange}
+                        />
+                    </div> 
+
+                {/* Agregar el Email */}
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={onChange}
+                        />
+                    </div>
+
+                {/* Agregar la Contraseña */}
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={onChange}
+                        />
+                    </div>
+
+                {/* Confirmar la Contraseña  */}
+                <div className="campo-form">
+                        <label htmlFor="confirmar">Confirmar Password</label>
+                        <input 
+                            type="password"
+                            id="confirmar"
+                            name="confirmar"
+                            placeholder="Confirma Tu Password"
+                            value={password}
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <input type="submit" className="btn btn-primario btn-block"
+                                value="Iniciar Sesión" />
+                    </div>
+                </form>
+                <Link to={'/nueva-cuenta'} className="enlace-cuenta">
+                    Obtener Cuenta
+                </Link>
+            </div>
+        </div>
      );
 }
  
-export default NuevaCuenta;
+export default NuevaCuenta
+
+
+

dca41fbb84f36211d03917b934b0f52a72449806
Jair <jairaceves56@gmail.com>
2021-11-09 20:15:04 -0600

    Haciendo la comunicacion entre paginas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Login from './components/auth/Login';
+import NuevaCuenta from './components/auth/NuevaCuenta';
+import Proyectos from './components/proyectos/Proyectos';
 
 function App() {
   return (
-    <h1>MERN TASK</h1>
+    <Router>
+    <Routes>
+        <Route exact path="/" element={<Login/>} />
+        <Route exact path="/nueva-cuenta" element={<NuevaCuenta/>} />
+        <Route exact path="/proyectos" element={<Proyectos/>} />
+    </Routes>
+</Router>
   );
 }
 

f2cc2259ce7aa95e255d1e499ea9c734413e00dd
Jair <jairaceves56@gmail.com>
2021-11-12 12:28:04 -0600

    Boton para borrar el Proyecto por completo

--- src/components/tareas/ListadoTarea.js
+++ src/components/tareas/ListadoTarea.js
@@ -25,7 +25,12 @@ const ListadoTareas = () => {
                         />
                     ))
                 }
+                
             </ul>
+            <button
+                type="button"
+                className="btn btn-eliminar"
+            >Eliminar Proyecto &times;</button>
         </Fragment>
      );
 }

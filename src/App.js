7bb13793d98369dd9ff255c08a208f650bec8d8e
Jair <jairaceves56@gmail.com>
2021-11-12 19:47:00 -0600

    Cambiando el contexto de False a True para ue este se muestre

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/auth/Login';
 import NuevaCuenta from './components/auth/NuevaCuenta';
 import Proyectos from './components/proyectos/Proyectos';
+import ProyectoState from './context/proyectos/proyectoState';
 
 function App() {
   return (
-    <Router>
-    <Routes>
-        <Route exact path="/" element={<Login/>} />
-        <Route exact path="/nueva-cuenta" element={<NuevaCuenta/>} />
-        <Route exact path="/proyectos" element={<Proyectos/>} />
-    </Routes>
-</Router>
+    <ProyectoState >
+      <Router>
+        <Routes>
+            <Route exact path="/" element={<Login/>} />
+            <Route exact path="/nueva-cuenta" element={<NuevaCuenta/>} />
+            <Route exact path="/proyectos" element={<Proyectos/>} />
+        </Routes>
+      </Router>
+    </ProyectoState>
   );
 }
 
 export default App;
+ 

b86fc85d6d585d4469761411f3c0acb60a215145
Jair <jairaceves56@gmail.com>
2021-11-13 11:57:38 -0600

    Ocultamos y Mostramos el Form para agregar tareas

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 const FormTarea = () => {
+
+    // Extraer si un proyecto esta Activo
+    const proyectosContext = useContext(proyectoContext);
+    const { proyecto } = proyectosContext;
+
+    // Si no hay proyecto seleccionado
+    if(!proyecto) return null;
+
+    // Desestructuracion de Arreglo para extraer el proyecto actual
+    const [proyectoAtual] = proyecto;
+
     return (
         <div className="formulario">
             <form>
